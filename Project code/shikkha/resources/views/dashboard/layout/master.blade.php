40c1847f9b999cf8bb17e162a7bb3bac1815113b
Maruf Billah <maruf.billah@northsouth.edu>
2020-08-01 23:40:17 +0600

    Create and Update Admin slidebar
    We create admin slidebar. Though its need some modification. But we work on it. And now it visible to our project. (BTW this is eid day)

--- Project code/shikkha/resources/views/dashboard/layout/master.blade.php
+++ Project code/shikkha/resources/views/dashboard/layout/master.blade.php
@@ -71,7 +71,7 @@
 			<!--begin::Page-->
 			<div class="d-flex flex-row flex-column-fluid page">
 				<!--begin::Aside-->
-				@include('dashboard.layout.institution.institution-sidebar')
+				@include('dashboard.layout.admin.admin-sidebar')
 				<!--end::Aside-->
 				<!--begin::Wrapper-->
 				<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
