bbd1116a684a14ce31977d4ed348663ae65dc4bb
bhsam787 <samarvin3@gmail.com>
2020-08-04 23:08:14 +0600

    simple css issue fix
    css fix on stream page

--- Project code/shikkha/resources/views/dashboard/student/stream.blade.php
+++ Project code/shikkha/resources/views/dashboard/student/stream.blade.php
@@ -47,24 +47,24 @@
                             <h3 class="card-label">
                                 CSE488A
                                 <small>16</small>
-                              </br> <span style="font-size:12px">Syed Athar Bin Amir</span>
+                                </br> <span style="font-size:12px">Syed Athar Bin Amir</span>
                             </h3>
                         </div>
 
-                        <div class="card-footer d-flex justify-content-between">
-                            <a href="#" class="btn btn-success font-weight-bold mr-2">Join</a>
+                        <div class="card-footer d-flex justify-content-between stream-button">
+                            <a href="#" class="btn btn-success font-weight-bold mr-2">Conference</a>
                             <a href="#" class="btn btn-info font-weight-bold mr-2">Steam</a>
-                        </div>                        
+                        </div>
 
-                        
+
                     </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                              <span class="d-block font-weight-bold mb-4 text-center">Class Time</span>
-                              <span class="btn btn-light-success btn-sm font-weight-bold btn-upper btn-text mt-2">Saturday (4:20-5:50)</span>
-                              <span class="btn btn-light-success btn-sm font-weight-bold btn-upper btn-text mt-2">Sunday (4:20-5:50)</span>
-                              <span class="btn btn-light-success btn-sm font-weight-bold btn-upper btn-text mt-2">Wednesday (4:20-5:50)</span>
+                                <span class="d-block font-weight-bold mb-4 text-center">Class Time</span>
+                                <span class="btn btn-light-success btn-sm font-weight-bold btn-upper btn-text mt-2">Saturday (4:20-5:50)</span>
+                                <span class="btn btn-light-success btn-sm font-weight-bold btn-upper btn-text mt-2">Sunday (4:20-5:50)</span>
+                                <span class="btn btn-light-success btn-sm font-weight-bold btn-upper btn-text mt-2">Wednesday (4:20-5:50)</span>
                             </div>
                             {{-- <div class="col-lg-4">
                               <p>hi</p>
@@ -75,11 +75,11 @@
                         </div>
                     </div>
                     <div class="card-footer d-flex justify-content-between">
-                        <div class="card-toolbar">                        
+                        <div class="card-toolbar">
                             <a href="#" class="btn btn-sm btn-icon btn-light-primary mr-2 tooltipicon">
                                 <i class="far fa-comments"></i>
                                 <span class="tooltipicontext">Course Discussion</span>
-                            </a>                            
+                            </a>
                             <a href="#" class="btn btn-sm btn-icon btn-light-warning mr-2 tooltipicon">
                                 <i class="far fa-calendar-check"></i>
                                 <span class="tooltipicontext">Class work</span>
@@ -93,28 +93,26 @@
                                 <span class="tooltipicontext">Notifications</span>
                             </a>
 
-                            <div class="symbol-group symbol-hover justify-content-between">
-                                <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Mark Stone">
-                                    <img alt="Pic" src="{{asset('assets/media/users/300_25.jpg') }}">
-                                </div>
-                                <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Charlie Stone">
-                                    <img alt="Pic" src="{{asset('assets/media/users/300_19.jpg') }}">
-                                </div>
-                                <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Luca Doncic">
-                                    <img alt="Pic" src="{{asset('assets/media/users/300_22.jpg') }}">
-                                </div>
-                                <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Nick Mana">
-                                    <img alt="Pic" src="{{asset('assets/media/users/300_23.jpg') }}">
-                                </div>
-                                <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Teresa Fox">
-                                    <img alt="Pic" src="{{asset('assets/media/users/300_18.jpg') }}">
-                                </div>
-                                <div class="symbol symbol-30 symbol-circle symbol-light" data-toggle="tooltip" title="" data-original-title="More users">
-                                    <span class="symbol-label font-weight-bold">35+</span>
-                                </div>
-							</div>
-
-                            
+                        </div>
+                        <div class="symbol-group symbol-hover justify-content-between">
+                            <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Mark Stone">
+                                <img alt="Pic" src="{{asset('assets/media/users/300_25.jpg') }}">
+                            </div>
+                            <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Charlie Stone">
+                                <img alt="Pic" src="{{asset('assets/media/users/300_19.jpg') }}">
+                            </div>
+                            <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Luca Doncic">
+                                <img alt="Pic" src="{{asset('assets/media/users/300_22.jpg') }}">
+                            </div>
+                            <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Nick Mana">
+                                <img alt="Pic" src="{{asset('assets/media/users/300_23.jpg') }}">
+                            </div>
+                            <div class="symbol symbol-30 symbol-circle" data-toggle="tooltip" title="" data-original-title="Teresa Fox">
+                                <img alt="Pic" src="{{asset('assets/media/users/300_18.jpg') }}">
+                            </div>
+                            <div class="symbol symbol-30 symbol-circle symbol-light" data-toggle="tooltip" title="" data-original-title="More users">
+                                <span class="symbol-label font-weight-bold">35+</span>
+                            </div>
                         </div>
                     </div>
                 </div>
