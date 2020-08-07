2bc6ad961ce0019c230e5923c04b72f57cbad4b3
Maruf Billah <maruf.billah@northsouth.edu>
2020-08-07 22:55:49 +0600

    delete a extra div
    I deleted a extra div from 2 pages

--- Project code/shikkha/resources/views/dashboard/student/pre-advising.blade.php
+++ Project code/shikkha/resources/views/dashboard/student/pre-advising.blade.php
@@ -3,41 +3,44 @@
 @section('section')
 
 <!--begin::Content-->
-<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
 
 
-	<!--begin::Entry-->
-	<div class="d-flex flex-column-fluid">
-		<!--begin::Container-->
-		<div class="container">
-			<!--begin::Card-->
-			<div class="card card-custom">
-				<!--begin::Header-->
-				<div class="card-header flex-wrap border-0 pt-6 pb-0">
-					<div class="card-title justify-content-between ">
-						<h3 class="card-label">Pre Advising
-							<span class="d-block text-muted pt-2 font-size-sm">This semeseter all course shown here</span></h3>
-					</div>
-					<div class="card-title justify-content-between ">
-						<h3 class="card-label">Course Duration
-							<span class="d-block text-muted pt-2 font-size-sm">10.11.2020- 10.11.2021</span></h3>
-					</div>
 
-				</div>
-				<!--end::Header-->
-				<!--begin::Body-->
-				<div class="card-body">
-					<!--begin: Datatable-->
-					
-					<!--end: Datatable-->
+<!--begin::Entry-->
 
-				<!--end::Body-->
-			</div>
-			<!--end::Card-->
-		</div>
-		<!--end::Container-->
-	</div>
-	<!--end::Entry-->
+<div class="d-flex flex-column-fluid">
+    <!--begin::Container-->
+    <div class="container">
+        <!--begin::Card-->
+        <div class="card card-custom">
+            <!--begin::Header-->
+            <div class="card-header flex-wrap border-0 pt-6 pb-0">
+                <div class="card-title justify-content-between ">
+                    <h3 class="card-label">Pre Advising
+                        <span class="d-block text-muted pt-2 font-size-sm">This semeseter all course shown here</span>
+                    </h3>
+                </div>
+                <div class="card-title justify-content-between ">
+                    <h3 class="card-label">Course Duration
+                        <span class="d-block text-muted pt-2 font-size-sm">10.11.2020- 10.11.2021</span>
+                    </h3>
+                </div>
+
+            </div>
+            <!--end::Header-->
+            <!--begin::Body-->
+            <div class="card-body">
+                <!--begin: Datatable-->
+
+                <!--end: Datatable-->
+
+                <!--end::Body-->
+            </div>
+            <!--end::Card-->
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::Entry-->
 </div>
 <!--end::Content-->
 
