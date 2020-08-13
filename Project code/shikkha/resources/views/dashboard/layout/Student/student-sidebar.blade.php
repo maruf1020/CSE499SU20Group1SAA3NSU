65c54bcc743bf3a2913bbf6d80acaf4f9a65d355
Maruf Billah <maruf.billah@northsouth.edu>
2020-08-13 19:58:51 +0600

    Update slidebar link
    I update slidebar link and add change pasword link

--- Project code/shikkha/resources/views/dashboard/layout/Student/student-sidebar.blade.php
+++ Project code/shikkha/resources/views/dashboard/layout/Student/student-sidebar.blade.php
@@ -114,7 +114,7 @@
 
                             </li>
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-                                <a href="javascript:;" class="menu-link menu-toggle">
+                                <a href="change-password" class="menu-link menu-toggle">
                                     <i class="menu-bullet menu-bullet-line">
                                         <span></span>
                                     </i>
