3010b620e94cdcc09bb9caf3f51e8da2402e906b
Maruf Billah <maruf.billah@northsouth.edu>
2020-08-23 23:30:45 +0600

    Edit student stream home page design
    I edit student stream home page design

--- Project code/shikkha/resources/views/dashboard/student/stream-home.blade.php
+++ Project code/shikkha/resources/views/dashboard/student/stream-home.blade.php
@@ -568,6 +568,80 @@
 							</div>
 							<div class="d-flex align-items-center justify-content-between flex-wrap mb-1 mt-5">
 								<div class="my-lg-0 my-1 mr-10">
+									<button type="reset" class="btn btn-light-warning mr-2">Do the task</button>
+									<button type="reset" class="btn btn-light-danger mr-2">Report</button>
+								</div>
+							</div>							
+							<div class="card-body pt-5 pb-5">
+							<h1 class="mb-4">MID-2 Exam</h1>
+								<div class="d-flex align-items-center justify-content-between mb-10">
+									<div class="d-flex align-items-center mr-2 pr-5 border">
+										<div class="symbol symbol-50 symbol-light mr-3 flex-shrink-0">
+											<div class="symbol-label">
+												<img src="assets/media/svg/files/pdf.svg" alt="" class="h-50">
+											</div>
+										</div>										
+										<div>
+											<a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Data analysis part II </a>
+											<div class="font-size-sm text-muted font-weight-bold mt-1">PDF</div>
+										</div>
+									</div>
+								</div>
+								<p>Weekly update format:
+
+									- Email sent to the instructor by Sunday Midnight. Update should include tasks undertaken, in progress, and completed over the week. This does not have a set format, but all group members must be cc-ed in it.
+
+									Those of you who failed to submit a proposal should email me ASAP explaining the circumstances for the delay, and complete and submit your proposals by tomorrow (4/8/2020).</p>
+							</div>
+						</div>
+						<!--end: Data table-->
+						<div class="d-flex align-items-center ml-15 mt-4">
+							<span class="svg-icon svg-icon-gray-500">
+								<!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Group-chat.svg-->
+								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+										<rect x="0" y="0" width="24" height="24"></rect>
+										<path
+										  d="M16,15.6315789 L16,12 C16,10.3431458 14.6568542,9 13,9 L6.16183229,9 L6.16183229,5.52631579 C6.16183229,4.13107011 7.29290239,3 8.68814808,3 L20.4776218,3 C21.8728674,3 23.0039375,4.13107011 23.0039375,5.52631579 L23.0039375,13.1052632 L23.0206157,17.786793 C23.0215995,18.0629336 22.7985408,18.2875874 22.5224001,18.2885711 C22.3891754,18.2890457 22.2612702,18.2363324 22.1670655,18.1421277 L19.6565168,15.6315789 L16,15.6315789 Z"
+										  fill="#000000"></path>
+										<path
+										  d="M1.98505595,18 L1.98505595,13 C1.98505595,11.8954305 2.88048645,11 3.98505595,11 L11.9850559,11 C13.0896254,11 13.9850559,11.8954305 13.9850559,13 L13.9850559,18 C13.9850559,19.1045695 13.0896254,20 11.9850559,20 L4.10078614,20 L2.85693427,21.1905292 C2.65744295,21.3814685 2.34093638,21.3745358 2.14999706,21.1750444 C2.06092565,21.0819836 2.01120804,20.958136 2.01120804,20.8293182 L2.01120804,18.32426 C1.99400175,18.2187196 1.98505595,18.1104045 1.98505595,18 Z M6.5,14 C6.22385763,14 6,14.2238576 6,14.5 C6,14.7761424 6.22385763,15 6.5,15 L11.5,15 C11.7761424,15 12,14.7761424 12,14.5 C12,14.2238576 11.7761424,14 11.5,14 L6.5,14 Z M9.5,16 C9.22385763,16 9,16.2238576 9,16.5 C9,16.7761424 9.22385763,17 9.5,17 L11.5,17 C11.7761424,17 12,16.7761424 12,16.5 C12,16.2238576 11.7761424,16 11.5,16 L9.5,16 Z"
+										  fill="#000000" opacity="0.3"></path>
+									</g>
+								</svg>
+								<!--end::Svg Icon-->
+							</span>
+							<a href="#" class="font-weight-bolder text-primary ml-2">6 Comments</a>
+						</div>
+						<div class="form-group mb-5 mt-5 ml-10 mr-10">
+							<textarea class="form-control" id="exampleTextarea" rows="2"></textarea>
+						</div>
+						<div class="d-flex align-items-center justify-content-between flex-wrap mb-10">
+							<div class="mr-3">
+
+							</div>
+							<div class="my-lg-0 my-1 mr-10">
+								<button type="reset" class="btn btn-light-primary mr-2">Submit</button>
+								<button type="reset" class="btn btn-danger">Cancel</button>
+
+							</div>
+						</div>
+					</div>
+					<!-- END::view old post with file -->
+					<!-- start::view old post with file  -->
+					<div class="card card-custom gutter-b border border-1">
+						<div class="card-header">
+							<div class="card-title mt-10">
+								<div class="symbol symbol-circle symbol-40 mr-3">
+									<img alt="Pic" src="assets/media/users/75_1.jpg">
+								</div>
+								<div>
+									<a href="#" class="text-dark-75 text-hover-primary font-weight-bold font-size-h6">Syed Athar Bin Amir</a>
+									<span class="text-muted font-size-sm">Aug 3</span>
+								</div>
+							</div>
+							<div class="d-flex align-items-center justify-content-between flex-wrap mb-1 mt-5">
+								<div class="my-lg-0 my-1 mr-10">
 									<button type="reset" class="btn btn-light-danger mr-2">Report</button>
 								</div>
 							</div>
