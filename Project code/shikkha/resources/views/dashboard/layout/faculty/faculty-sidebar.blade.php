c23901adfbeb91ed265bdca8cd7a6cccbaa46086
Maruf Billah <maruf.billah@northsouth.edu>
2020-09-01 12:11:52 +0600

    Update faculty sidebar link
    I updated the faculty sidebar link

--- Project code/shikkha/resources/views/dashboard/layout/faculty/faculty-sidebar.blade.php
+++ Project code/shikkha/resources/views/dashboard/layout/faculty/faculty-sidebar.blade.php
@@ -310,7 +310,7 @@
                         <ul class="menu-subnav">
 
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-                                <a href="javascript:;" class="menu-link menu-toggle">
+                                <a href="faculty-notifications" class="menu-link menu-toggle">
                                     <i class="menu-bullet menu-bullet-line">
                                         <span></span>
                                     </i>
@@ -320,7 +320,7 @@
                             </li>
 
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-                                <a href="javascript:;" class="menu-link menu-toggle">
+                                <a href="faculty-message" class="menu-link menu-toggle">
                                     <i class="menu-bullet menu-bullet-line">
                                         <span></span>
                                     </i>
